a7c6f84b0f05a5a5868154ff13b5598c01029d7c
Kristof <kredei@users.noreply.github.com>
2022-01-23 17:54:55 -0500

    First commit

---
+++ javascripts/site.js
@@ -0,0 +1,16 @@
+//document.addEventListener("DOMContentLoaded", function() {
+//  document.querySelectorAll("[data-click-action='case-study']").forEach(function(button) {
+//    button.addEventListener("click",
+//      function(event) {
+//        var sectionToShow = event.target.dataset.section;
+//        var caseStudySection = document.querySelector("section" + sectionToShow);
+//        if (caseStudySection.style.opacity === 0) {
+//          caseStudySection.style.opacity = 1;
+//        }
+//      }
+//    );
+//  });
+//
+//}, false);
+
+
